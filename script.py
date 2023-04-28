0fa69ecc1ab4383f3749876ee734587736881e4a
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2023-04-28 05:14:37 -0400

    Update
    - Seen messages are now saved to a json file
    - Simplify code

--- script.py
+++ script.py
@@ -10,43 +10,67 @@ username = os.environ.get("IG_USERNAME")
 email = os.environ.get("IG_EMAIL")
 password = os.environ.get("IG_PASSWORD")
 
+def authenticate(client, session_file):
+    if os.path.exists(session_file):
+        client.load_settings(session_file)
+        try:
+            client.login(username, password)
+            client.get_timeline_feed()  # check if the session is valid
+        except LoginRequired:
+            # session is invalid, re-login and save the new session
+            client.login(username, password)
+            client.dump_settings(session_file)
+    else:
+        client.login(username, password)
+        client.dump_settings(session_file)
+
+
+def load_seen_messages(file):
+    if os.path.exists(file):
+        with open(file, "r") as f:
+            return set(json.load(f))
+    else:
+        return set()
+
+
+def save_seen_messages(file, messages):
+    with open(file, "w") as f:
+        json.dump(list(messages), f)
+
+
 cl = Client()
 cl.delay_range = [1, 3]
 
 session_file = "session.json"
+seen_messages_file = "seen_messages.json"
 
-if os.path.exists(session_file):
-    cl.load_settings(session_file)
-    try:
-        cl.login(username, password)
-        cl.get_timeline_feed()  # check if the session is valid
-    except LoginRequired:
-        # session is invalid, re-login and save the new session
-        cl.login(username, password)
-        cl.dump_settings(session_file)
-else:
-    cl.login(username, password)
-    cl.dump_settings(session_file)
+authenticate(cl, session_file)
 
 user_id = cl.user_id_from_username(username)
-print(f"The user id is {user_id}")
+print(f"Logged in as user ID {user_id}")
 
-# Initialize an empty set to store the ids of the messages already seen
-seen_message_ids = set()
+cl.delay_range = [1, 3]
+
+seen_message_ids = load_seen_messages(seen_messages_file)
 
 while True:
     threads = cl.direct_threads()
+    cl.delay_range = [1, 3]
     for thread in threads:
         thread_id = thread.id
         messages = cl.direct_messages(thread_id)
+        cl.delay_range = [1, 3]
         for message in messages:
             if message.id not in seen_message_ids:
                 match message.item_type:
-                    case 'clip':
-                        print(f"New reel in thread {thread_id}: {message.clip.video_url}")
-                    case 'xma_story_share':
+                    case "clip":
+                        print(
+                            f"New reel in thread {thread_id}: {message.clip.video_url}"
+                        )
+                    case "xma_story_share":
                         print(f"New story video in thread {thread_id}: {message.id}")
                     case _:
                         print(f"New message in thread {thread_id}: {message.text}")
                 seen_message_ids.add(message.id)
-    time.sleep(3)  # check for new messages every 10 seconds
+                save_seen_messages(seen_messages_file, seen_message_ids)
+    time.sleep(30)  # check for new messages every 30 seconds
